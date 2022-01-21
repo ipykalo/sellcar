580de3efdf095f7acb61c1dfd71a4afb5e3d9cbd
Ivan <ipyka>
2022-01-21 11:50:07 +0200

    added entities

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ReportsService } from './reports.service';
 import { ReportsController } from './reports.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Report } from './report.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Report])
+  ],
   providers: [ReportsService],
   controllers: [ReportsController]
 })
-export class ReportsModule {}
+export class ReportsModule { }
