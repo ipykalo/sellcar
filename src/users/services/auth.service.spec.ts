c440c004591144a25621d27d429ec606e731cdc2
Ivan <ipyka>
2022-01-25 16:02:37 +0200

    added test case for 'signin'

--- src/users/services/auth.service.spec.ts
+++ src/users/services/auth.service.spec.ts
@@ -24,18 +24,23 @@ describe('AuthService', () => {
     authService = module.get<AuthService>(AuthService);
   });
 
+  let mockedUser: Partial<User>;
+
+  beforeEach(async () => {
+    const hashPassword = await bcrypt.hash('12345', 10);
+    mockedUser = { id: 1, email: 'test@gmail.com', password: hashPassword };
+  });
+
   it('should be defined', () => {
     expect(authService).toBeDefined();
   });
 
-  describe('signup', () => {
-    const mockedUser = { id: 1, email: 'test@gmail.com', password: '12345'};
+  describe('signup()', () => {
 
     it('should throw an error if email already exists', () => {
       jest.spyOn(UserService.prototype, 'find').mockResolvedValue([mockedUser] as User[]);
-      
+
       authService.signup(mockedUser.email, mockedUser.password)
-        .then()
         .catch(err => {
           expect(err.response.message).toEqual('A user with the email already exists.');
         });
@@ -43,14 +48,41 @@ describe('AuthService', () => {
 
     it('should create a user', () => {
       jest.spyOn(UserService.prototype, 'find').mockResolvedValue([] as User[]);
-      const bcryptHash = jest.fn().mockResolvedValue('hashed-pasword');
-      (bcrypt.hash as jest.Mock) = bcryptHash;
       jest.spyOn(UserService.prototype, 'create').mockResolvedValue(mockedUser as User);
 
       authService.signup(mockedUser.email, mockedUser.password)
         .then(user => {
           expect(user.email).toEqual(mockedUser.email);
         });
+    });
+  });
+
+  describe('signin()', () => {
+
+    it('should throw error if user doesn\'t exist', () => {
+      jest.spyOn(UserService.prototype, 'find').mockResolvedValue([] as User[]);
+
+      authService.signin(mockedUser.email, mockedUser.password)
+        .catch(err => {
+          expect(err.response.message).toEqual('A user with the email does not exist.');
+        });
+    });
+
+    it('should throw error if password doesn\'t match', () => {
+      jest.spyOn(UserService.prototype, 'find').mockResolvedValue([mockedUser] as User[]);
+      authService.signin(mockedUser.email, '123456')
+        .catch(err => {
+          expect(err.response.message).toEqual('Entered pasword is incorect.');
+        });
+    });
+
+    it('should login a user', () => {
+      jest.spyOn(UserService.prototype, 'find').mockResolvedValue([mockedUser] as User[]);
+      authService.signin(mockedUser.email, '12345')
+        .then(user => {
+          expect(user.password).toEqual(mockedUser.password);
+        })
+        .catch(err => expect(err).toBeFalsy());
     });
   });
 });
